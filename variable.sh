0f808818f1e68d14e5ab97d492c0f19ea9f5e417
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-10-14 19:33:41 +0530

    date

--- variable.sh
+++ variable.sh
@@ -4,8 +4,12 @@ name=Manju
 echo a = ${a}
 echo  My name is = ${name}
 
+#variable to write a command
 date=$(date +%F)
-#
 echo today the date ${date}
+
+#variable to write some arthemetic logic
+maths=$((2+4-1))
+echo solution $maths
 
 
