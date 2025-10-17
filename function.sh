5b63a76c4d27006f4d85d173ebfa13da504c87ae
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-10-17 21:53:06 +0530

    script path

--- function.sh
+++ function.sh
@@ -6,3 +6,14 @@ fucntion_name(){
 
 # to call a function
 fucntion_name
+
+#we can also give inouts to the functions
+
+func(){
+  echo first argument = $1
+  echo second argument = $2
+  echo all arguments = $*
+  echo number of arguments = $#
+}
+
+func 123 xyz
