083a5bb7524c596e69de486eae7a294b0a1564a5
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-10-18 23:13:30 +0530

    conditions practice

--- conditions.sh
+++ conditions.sh
@@ -17,3 +17,12 @@ then
 else
   echo name does not exsist
 fi
+
+#alwasy double quate the varialble in expressions to avoid failures
+
+if [ $age -gt 25 ]
+then
+  echo $name is a old person
+else
+  echo $name is a youngest person
+fi
