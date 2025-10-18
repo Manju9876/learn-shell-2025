0b0887168a2aa616f9830c5ba419a97a10278067
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-10-18 20:36:55 +0530

    conditions practice

--- conditions.sh
+++ conditions.sh
@@ -5,3 +5,15 @@ if [ $fruit_name = "mango" ]
 then
   echo the quantity is $quantity
 fi
+
+# example for if else
+
+name=$1
+age=$2
+
+if [ name = "Manju"]
+then
+  echo he is a super hero
+else
+  echo who is manju
+fi
