aa9371d8c9948974a7fc1e9430179d4fb61e9868
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-10-27 22:39:54 +0530

    loops concepts and practice

--- loops.sh
+++ loops.sh
@@ -1,6 +1,11 @@
 #two types of loops
 #while loop based on the expression
-
+#runs as long as the contition is true
+count=5
+while [${count} -gt 1]; do
+  echo "count=${count}"
+  count=$((count-1))
+done
 #for loop based on the input
 #for variable in list
 #do
