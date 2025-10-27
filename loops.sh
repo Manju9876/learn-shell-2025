1982ba7469b6e178837a006ee47780a9a95ec563
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-10-27 22:34:45 +0530

    loops concepts and practice

---
+++ loops.sh
@@ -0,0 +1,11 @@
+#two types of loops
+#while loop based on the expression
+
+#for loop based on the input
+#for variable in list
+#do
+#   commands
+#done
+for i in 1 2 3 ; do
+  echo number: ${i}
+done
